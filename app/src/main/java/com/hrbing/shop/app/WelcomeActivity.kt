8a35fd8ae3189aeb6afbd6d77b5093096fe11bd9
ruibing.han <ruibing.han@externals.adidas.com>
2019-04-20 16:42:09 +0800

    add welcome page

---
+++ app/src/main/java/com/hrbing/shop/app/WelcomeActivity.kt
@@ -0,0 +1,10 @@
+package com.hrbing.shop.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WelcomeActivity() : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ test-data/src/main/java/com/hrbing/shop/test/TestDataActivity.kt
@@ -0,0 +1,10 @@
+package com.hrbing.shop.test
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TestDataActivity() : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
