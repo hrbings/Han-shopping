4161a0e431a7f1050a545981e7e5616aff37bc32
ruibing.han <ruibing.han@externals.adidas.com>
2019-04-28 08:36:55 +0800

    move module values,font to module-ui

--- core-ui/src/main/java/com/hrbing/shop/core/ui/font/FontUtil.kt
+++ core-ui/src/main/java/com/hrbing/shop/core/ui/font/FontUtil.kt
@@ -1,8 +1,9 @@
-package com.hrbing.shop.font
+package com.hrbing.shop.core.ui.font
 
 import android.content.Context
 import android.graphics.Typeface
 import androidx.core.content.res.ResourcesCompat
+import com.hrbing.shop.core.ui.R
 
 /**
  * @author ...
